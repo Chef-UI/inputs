7daa7792a77fbd71bcdd8212ec77c7b457d347cf
cl4pper <cl4pper@gmail.com>
2021-11-28 02:19:29 +0000

    feat: create template component

---
+++ src/Template/component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+// STYLES
+import './style.scss';
+
+const Template = () => {
+    return (
+        <p className="Template">Template component</p>
+    )
+}
+
+export default Template;
