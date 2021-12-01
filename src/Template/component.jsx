d697cee9b6e65da61b61fb101a7df30cd7d04b35
cl4pper <cl4pper@gmail.com>
2021-12-01 11:25:33 +0000

    chore: setup pre-commit hook for testing

--- src/Template/component.jsx
+++ src/Template/component.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 // STYLES
 import './style.scss';
 
-const Template = () => {
-	return <p className="Template">Template component</p>;
+const Template = (props) => {
+	const { subtitle } = props;
+
+	const renderSubtitle = () => 'Template component is cool!';
+
+	return (
+		<div className="Template">
+			<p className="Template__title">Template component</p>
+			<p className="Template__subtitle">{subtitle && renderSubtitle()}</p>
+		</div>
+	);
+};
+
+Template.propTypes = {
+	subtitle: PropTypes.bool,
 };
 
 export default Template;
