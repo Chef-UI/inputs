afe21f32c9310bd512f5c7a0b0ac497b5b00722e
cl4pper <cl4pper@gmail.com>
2021-12-13 22:54:18 +0000

    feat: remove Template component

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,6 @@
 // const path = require('path');
 
 module.exports = {
-    stories: ["../src/components/**/*.story.jsx"],
-    addons: ["@storybook/addon-postcss"]
+	stories: ['../src/components/**/*.story.jsx'],
+	addons: ['@storybook/addon-postcss'],
 };
